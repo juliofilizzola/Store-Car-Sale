7a2870cfe878b3aba628b145054ebdbf862b799c
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-12 18:53:39 -0300

    feat: finely car post controller

--- src/main/java/com/store/car/controller/CarPostController.java
+++ src/main/java/com/store/car/controller/CarPostController.java
@@ -5,7 +5,6 @@ import com.store.car.service.CarPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/sales")
 public class CarPostController {
+
     @Autowired
     private CarPostService carPostService;
 
@@ -21,9 +21,17 @@ public class CarPostController {
         return ResponseEntity.status(HttpStatus.FOUND).body(carPostService.getCarSales());
     }
 
-    @GetMapping("/car/{id}")
+    @PutMapping("/car/{id}")
     public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id) {
         carPostService.changeCarSale(carPostDTO, Long.valueOf(id));
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @DeleteMapping("/car/{id}")
+    public ResponseEntity removeCarSale(@PathVariable("id") String id) {
+        carPostService.removerCarSale(Long.valueOf(id));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
